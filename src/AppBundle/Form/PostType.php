82a744243bda7d95c124b4d77d5d859d32b59b05
Reybeka Peyrat <reybekapeyrat@gmail.com>
2020-06-13 12:51:41 +0200

    pagination articles/ published articles

--- src/AppBundle/Form/PostType.php
+++ src/AppBundle/Form/PostType.php
@@ -7,7 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\DateTime;
+
 
 class PostType extends AbstractType
 {
@@ -17,8 +17,15 @@ class PostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', TextType::class)
-            ->add('content', TextType::class)
+            ->add('title', TextType::class,[
+                'label' => 'Titre'
+            ])
+            ->add('content', TextType::class,[
+                'label' => 'Contenu'
+            ])
+            ->add('author', TextType::class,[
+                'label' => 'Auteur'
+            ])
             ->add('published', CheckboxType::class,[
                     'label' => 'publié'
                 ])
